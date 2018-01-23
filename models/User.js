a2555ed83f8ecd9612ead0af6329909ac7e31cd7
JamieRez <jamierezendes@gmail.com>
2018-01-23 12:09:53 -0800

    Added Register and Login Post Endpoints

--- models/User.js
+++ models/User.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 
-let userSchema = new mongoose.schema({
+let userSchema = new mongoose.Schema({
   username : String,
   password : String,
   email : String,
-  following : [userSchema],
-  followers : [userSchema]
+  following : [String],
+  followers : [String]
 })
+
+userSchema.methods.hashPassword = (pass) => {
+  return bcrypt.hashSync(pass, bcrypt.genSaltSync(8));
+}
+
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
 
 let User = mongoose.model('User', userSchema);
 
--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const app = require('express')();
 var server = require('http').Server(app)
 var io = require('socket.io')(server)
 const mongoose = require('mongoose');
-
-
+const bodyParser = require('body-parser');
 const port = process.env.PORT || '6969';
+const User = require('./models/User');
 
 mongoose.connect(process.env.MONGO_URI || 'localhost:27017/podcore-db');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+
 
 var rooms = {}
 
@@ -18,57 +23,39 @@ app.get('/rooms', function(req, res) {
   res.send(roomList)
 })
 
+
 io.on('connection', function(socket) {
-
-  socket.on('create_room', function(room) {
-    if (!room.key) {
-      return
-    }
-    console.log('create room:', room)
-    var roomKey = room.key
-    rooms[roomKey] = room
-    socket.roomKey = roomKey
-    socket.join(roomKey)
-    console.log(rooms);
-  })
-
-  socket.on('close_room', function(roomKey) {
-    console.log('close room:', roomKey)
-    delete rooms[roomKey]
-  })
-
-  socket.on('disconnect', function() {
-    console.log('disconnect:', socket.roomKey)
-    if (socket.roomKey) {
-      delete rooms[socket.roomKey]
-    }
-  })
-
-  socket.on('join_room', function(roomKey) {
-    console.log('join room:', roomKey)
-    socket.join(roomKey)
-  })
-
-  socket.on('upvote', function(roomKey) {
-    console.log('upvote:', roomKey)
-    io.to(roomKey).emit('upvote')
-  })
-
-  socket.on('gift', function(data) {
-    console.log('gift:', data)
-    io.to(data.roomKey).emit('gift', data)
-  })
-
-  socket.on('comment', function(data) {
-    console.log('comment:', data)
-    io.to(data.roomKey).emit('comment', data)
-  })
-
+  require('./sockets/stream');
 })
 
 app.get('/', (req, res) => {
   res.send('Kash eats Lemons.');
 });
+
+app.post('/register', (req, res) => {
+  let newUser = new User({
+    username : req.body.username,
+    email : req.body.email,
+  });
+  newUser.password = newUser.hashPassword(req.body.password);
+  newUser.save();
+  res.send(newUser);
+});
+
+app.post('/login', (req, res) => {
+  User.findOne({username : req.body.username}, (err, user) => {
+    if(err){console.log(err)}
+    if (!user) {
+      res.status(404).send("No User with Username: " + req.body.username);
+    }else{
+      if(user.validPassword(req.body.password)){
+        res.send(user);
+      }else{
+        res.status(404).send("Wrong Password");
+      }
+    }
+  })
+})
 
 
 server.listen(port, () => {
