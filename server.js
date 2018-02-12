abdf8ac1a6470dbb5ccb09c922267440003591b6
JamieRez <jamierezendes@gmail.com>
2018-02-12 12:12:10 -0800

    Test

--- server.js
+++ server.js
@@ -90,8 +90,8 @@ app.post('/login', (req, res) => {
 app.get('/followers', (req, res) => {
 	console.log(req.headers.username);
 	User.findOne({username : req.headers.username}, (err, user) => {
-		console.log(user);
 		followerSet = new Set(user.followers);
+		console.log(followerSet);
 		res.json({followers : followerSet});
 	})
 })
