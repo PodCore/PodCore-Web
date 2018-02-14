9e6797756dc91f7eebc01385c9a618b901633ea8
James Rezendes <James@MacBook-Pro-2.local>
2018-02-14 12:08:06 -0800

    Sort test

--- sockets/stream.js
+++ sockets/stream.js
@@ -2,7 +2,7 @@ let User = require('../models/User');
 module.exports = (io, socket, rooms) => {
 
   socket.on('get_rooms', () => {
-    const roomList = Object.keys(rooms).map((key) => { return rooms });
+    let roomList = Object.keys(rooms).map((key) => { return rooms });
     roomList.sort((a,b) => {
       return a.viewCount < b.viewCount;
     });
