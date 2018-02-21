6ec7c8f8f9ff309640080a213f48819833d59adc
JamieRez <jamierezendes@gmail.com>
2018-02-21 13:05:02 -0800

    Rooms Layout Alpha

---
+++ controllers/rooms.js
@@ -0,0 +1,14 @@
+module.exports = (app, rooms) => {
+
+  app.get('/rooms', (req, res) => {
+  	const roomList = Object.keys(rooms).map((key) => { return rooms[key] })
+  	console.log(`ROOM LIST IS: ${roomList}`);
+  	res.send(roomList);
+  });
+
+  app.get('/rooms/:owner', (req, res) => {
+  	let owner = req.params.owner;
+  	res.json(rooms[owner]);
+  })
+
+}
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,18 @@
-export default function main() {
-  console.log("Test");
+export default function main(io, socket, $) {
+  $(document).ready(()=>{
+    socket.emit("get_rooms");
+    socket.on("get_rooms", (rooms) => {
+      rooms.forEach((room) => {
+        let newRoom = $('.roomProtoType').clone()
+        newRoom.removeClass('roomProtoType');
+        newRoom.appendTo('.roomsContainer');
+        newRoom.addClass('room');
+        newRoom.find('.roomName').text(room.name);
+        newRoom.find('.roomOwner').text(room.owner);
+        newRoom.find('.roomThumbUrl').attr('src', "http://blogdailyherald.com/wp-content/uploads/2014/10/wallpaper-for-facebook-profile-photo.jpg");
+        newRoom.find('.roomLikeCount').text(room.likes + " Likes");
+        newRoom.find('.roomViewCount').text(room.viewCount + " Views");
+      })
+    })
+  })
 }
