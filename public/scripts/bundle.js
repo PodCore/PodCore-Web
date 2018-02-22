a28df4b3f328a0dae1a25a99ffbfe79b25e99994
James Rezendes <James@MacBook-Pro-2.local>
2018-02-21 18:44:36 -0800

    New jquery

--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -160,11 +160,11 @@ function room(io, socket, $, AgoraRTC) {
 
     var stream = AgoraRTC.createStream({streamID: streamId, audio:true, video:true, screen:false});
 
-    client.join(streamId, undefined, function(uid) {
-        console.log("client" + uid + "joined channel");
-        //create local stream
+    client.init(streamId, function() {
+        console.log("client initialized");
+
     }, function(err) {
-        console.log("client join failed ", err);
+        console.log("client init failed ", err);
         //error handling
     });
 
