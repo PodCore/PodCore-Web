3b14e0b6f56ce614cecdf698a27f3237f0958809
JamieRez <jamierezendes@gmail.com>
2018-02-22 14:07:07 -0800

    wat

--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -146,37 +146,6 @@ function main(io, socket, $, AgoraRTC) {
 /* harmony export (immutable) */ __webpack_exports__["a"] = room;
 function room(io, socket, $, AgoraRTC) {
 
-  //Get Agora Client Connected and Get their Devices
-  let client = AgoraRTC.createClient({mode: 'interop'});
-  // AgoraRTC.getDevices((devices) =>{
-  //   console.log(devices);
-  //   var dev_count = devices.length;
-  //   var id = devices[0].deviceId;
-  // });
-
-  let streamId = $('.roomId').text();
-  let streamName = $('.roomName').text();
-
-  let appId = "6a05c965b5644b508eae5db13c82fdba";
-
-
-  client.init(appId, function() {
-      console.log("client initialized");
-      client.join(streamId, streamName , null, function(uid) {
-        console.log("User " + uid + " join channel successfully");
-        // let stream = AgoraRTC.createStream({streamID: uid, audio:true, video:true, screen:false})
-        // stream.init(() => {
-        //   console.log("Test");
-        //   stream.play('video');
-        // });
-      }, function(err){
-        console.log("join failed");
-      })
-      //join channel
-  }, function(err) {
-      console.log("client init failed ", err);
-      //error handling
-  });
 
 
   // stream.init(()=>{
@@ -190,6 +159,39 @@ function room(io, socket, $, AgoraRTC) {
 
 
   $(document).ready(() => {
+    //Get Agora Client Connected and Get their Devices
+    let client = AgoraRTC.createClient({mode: 'interop'});
+    // AgoraRTC.getDevices((devices) =>{
+    //   console.log(devices);
+    //   var dev_count = devices.length;
+    //   var id = devices[0].deviceId;
+    // });
+
+    let streamId = $('.roomId').text();
+    let streamName = $('.roomName').text();
+
+    let appId = "6a05c965b5644b508eae5db13c82fdba";
+
+    console.log(streamName);
+
+    client.init(appId, function() {
+        console.log("client initialized");
+        console.log(streamId);
+        client.join(streamId, streamName , null, function(uid) {
+          console.log("User " + uid + " join channel successfully");
+          // let stream = AgoraRTC.createStream({streamID: uid, audio:true, video:true, screen:false})
+          // stream.init(() => {
+          //   console.log("Test");
+          //   stream.play('video');
+          // });
+        }, function(err){
+          console.log("join failed");
+        })
+        //join channel
+    }, function(err) {
+        console.log("client init failed ", err);
+        //error handling
+    });
 
   })
 
