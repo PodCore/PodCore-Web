13c883c3dcff45a35dc26655487caabe7c82833f
James Rezendes <James@MacBook-Pro-2.local>
2018-02-21 20:09:17 -0800

    test

--- public/scripts/room.js
+++ public/scripts/room.js
@@ -18,18 +18,16 @@ export default function room(io, socket, $, AgoraRTC) {
 
     client.init(appId, function() {
         console.log("client initialized");
-        client.join(streamId, undefined, function(uid) {
-            console.log("client" + uid + "joined channel");
-
-        }, function(err) {
-            console.log("client join failed ", err);
-            //error handling
-        });
+        console.log(streamId);
         //join channel
     }, function(err) {
         console.log("client init failed ", err);
         //error handling
     });
+
+    client.subscribe(stream, function(err) {
+      console.log("Test");
+    })
 
   })
 
