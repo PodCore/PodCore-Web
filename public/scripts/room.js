09d682a74337425b74de5eba8684b8b8b62c6a20
James Rezendes <James@MacBook-Pro-2.local>
2018-02-22 13:55:37 -0800

    Test

--- public/scripts/room.js
+++ public/scripts/room.js
@@ -9,19 +9,20 @@ export default function room(io, socket, $, AgoraRTC) {
   // });
 
   let streamId = $('.roomId').text();
+  let streamName = $('.roomName').text();
 
   let appId = "6a05c965b5644b508eae5db13c82fdba";
-  let stream = AgoraRTC.createStream({streamID: streamId, audio:true, video:true, screen:false})
 
 
   client.init(appId, function() {
       console.log("client initialized");
-      client.join(streamId, appId, null, function(uid) {
+      client.join(streamId, streamName , null, function(uid) {
         console.log("User " + uid + " join channel successfully");
-        stream.init(() => {
-          console.log("Test");
-          stream.play('video');
-        });
+        // let stream = AgoraRTC.createStream({streamID: uid, audio:true, video:true, screen:false})
+        // stream.init(() => {
+        //   console.log("Test");
+        //   stream.play('video');
+        // });
       }, function(err){
         console.log("join failed");
       })
