e8eb47a0b3ea597152e74fe90902e94fc926e63f
JamieRez <jamierezendes@gmail.com>
2018-02-21 18:47:00 -0800

    Test

--- public/scripts/room.js
+++ public/scripts/room.js
@@ -14,9 +14,8 @@ export default function room(io, socket, $, AgoraRTC) {
 
     var stream = AgoraRTC.createStream({streamID: streamId, audio:true, video:true, screen:false});
 
-    client.init(streamId, function() {
-        console.log("client initialized");
-
+    client.join(streamId, undefined, function(uid) {
+        console.log("client " + uid + " initialized");
     }, function(err) {
         console.log("client init failed ", err);
         //error handling
