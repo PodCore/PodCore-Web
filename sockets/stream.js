9a510de6d8496774158ee6b3961687261f412dcb
JamieRez <jamierezendes@gmail.com>
2018-03-14 12:35:09 -0700

    Fixed Merge Conflict Residue

--- sockets/stream.js
+++ sockets/stream.js
@@ -6,26 +6,12 @@ module.exports = (io, socket, rooms) => {
     roomList.sort((a,b) => {
       return a.viewCount < b.viewCount;
     });
-<<<<<<< HEAD
-<<<<<<< HEAD
     console.log(roomList);
-=======
-    //console.log(roomList);
->>>>>>> 8492cbf6fb9e131d15bc2c72dbeb9a62a4444dca
-=======
-    console.log(roomList);
->>>>>>> b15054f31f33692e4e9430ba1a99a8154d9d49e9
     socket.emit('get_rooms', roomList);
   });
 
   socket.on("create_room", (data) => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
     console.log('created room:', data.name)
->>>>>>> 8492cbf6fb9e131d15bc2c72dbeb9a62a4444dca
-=======
->>>>>>> b15054f31f33692e4e9430ba1a99a8154d9d49e9
     rooms[data.owner] = {
       name : data.name,
       id : data.id,
@@ -34,31 +20,14 @@ module.exports = (io, socket, rooms) => {
       viewers : [],
       viewCount : 0,
       likes : 0,
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b15054f31f33692e4e9430ba1a99a8154d9d49e9
       likers : [],
       emojiGram : {},
       image : data.image,
       location : data.location
-<<<<<<< HEAD
     }
     socket.owner = data.owner
     socket.join(data.owner);
     console.log('New Room:', rooms[data.owner])
-=======
-      image : data.image
-    }
-    socket.owner = data.owner
-    socket.join(data.owner);
->>>>>>> 8492cbf6fb9e131d15bc2c72dbeb9a62a4444dca
-=======
-    }
-    socket.owner = data.owner
-    socket.join(data.owner);
-    console.log('New Room:', rooms[data.owner])
->>>>>>> b15054f31f33692e4e9430ba1a99a8154d9d49e9
     io.emit('new_room', (rooms[data.owner]));
   })
 
@@ -75,15 +44,7 @@ module.exports = (io, socket, rooms) => {
   socket.on('disconnect', function() {
     if (socket.owner) {
       console.log('disconnect:', socket.owner)
-<<<<<<< HEAD
-<<<<<<< HEAD
       delete rooms[socket.owner]
-=======
-      //delete rooms[socket.owner]
->>>>>>> 8492cbf6fb9e131d15bc2c72dbeb9a62a4444dca
-=======
-      delete rooms[socket.owner]
->>>>>>> b15054f31f33692e4e9430ba1a99a8154d9d49e9
       io.emit('remove_room', socket.owner);
     }
   })
@@ -92,10 +53,6 @@ module.exports = (io, socket, rooms) => {
     console.log(data.username + " has joined room " + rooms[data.owner].name)
     rooms[data.owner].viewers.push(data.username);
     rooms[data.owner].viewCount += 1;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b15054f31f33692e4e9430ba1a99a8154d9d49e9
     socket.join(data.owner);
   })
 
@@ -105,7 +62,6 @@ module.exports = (io, socket, rooms) => {
     rooms[data.owner].viewers.pop(userIndex, 1);
     rooms[data.owner].viewCount -= 1;
     socket.leave(data.owner);
-<<<<<<< HEAD
   })
 
   socket.on('upvote', function(data) {
@@ -116,6 +72,7 @@ module.exports = (io, socket, rooms) => {
       io.to(data.owner).emit('upvote', {likes : rooms[data.owner].likes})
     }
   })
+
 
   socket.on('emoji', function(data) {
     console.log(data.owner + ": " + data.emojier + ": " + data.emojiNum);
@@ -129,54 +86,12 @@ module.exports = (io, socket, rooms) => {
       emojiNum : data.emojiNum,
       emojiGram : rooms[data.owner].emojiGram
     });
-=======
-    socket.join(rooms[data.owner].id);
-=======
->>>>>>> b15054f31f33692e4e9430ba1a99a8154d9d49e9
-  })
-
-  socket.on('upvote', function(data) {
-    if(!rooms[data.owner].likers.includes(data.upvoter)){
-      console.log('upvote:', data.owner + ": " + (rooms[data.owner].likes + 1))
-      rooms[data.owner].likes += 1;
-      rooms[data.owner].likers.push(data.upvoter);
-      io.to(data.owner).emit('upvote', {likes : rooms[data.owner].likes})
-    }
-  })
-
-<<<<<<< HEAD
-  socket.on('gift', function(data) {
-    console.log('gift:', data)
-    io.to(data.roomKey).emit('gift', data)
->>>>>>> 8492cbf6fb9e131d15bc2c72dbeb9a62a4444dca
-=======
-  socket.on('emoji', function(data) {
-    console.log(data.owner + ": " + data.emojier + ": " + data.emojiNum);
-    if(data.emojiNum in rooms[data.owner].emojiGram){
-      rooms[data.owner].emojiGram[data.emojiNum] += 1;
-    }else{
-      rooms[data.owner].emojiGram[data.emojiNum] = 1;
-    }
-    io.to(data.owner).emit('emoji', {
-      emojier : data.emojier,
-      emojiNum : data.emojiNum,
-      emojiGram : rooms[data.owner].emojiGram
-    });
->>>>>>> b15054f31f33692e4e9430ba1a99a8154d9d49e9
   })
 
   socket.on('comment', function(data) {
     console.log(data.owner + "'s room: " + data.commenter + ": " + data.comment);
     commentData = {comment : data.comment, commenter : data.commenter};
-<<<<<<< HEAD
-<<<<<<< HEAD
     io.to(data.owner).emit('comment', {commenter : data.commenter, comment : data.comment});
-=======
-    io.to(data.owner).emit('comment', commentData);
->>>>>>> 8492cbf6fb9e131d15bc2c72dbeb9a62a4444dca
-=======
-    io.to(data.owner).emit('comment', {commenter : data.commenter, comment : data.comment});
->>>>>>> b15054f31f33692e4e9430ba1a99a8154d9d49e9
   })
 
   socket.on('get_user', (username) => {
